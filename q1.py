9dc8b602b429a6e05326be8cecc629b9bf85218a
syeda aroosa <85955000+laiba312@users.noreply.github.com>
2024-08-07 18:18:50 +0500

    q1.py

--- q1.py
+++ q1.py
@@ -1,11 +1,27 @@
-# Prompt the user to enter the first number
-first_number = int(input("Enter the first number: "))
+# Problem Statement: Write a program to solve this age-related riddle! Anton, Beth, Chen, Drew, and Ethan are all friends. Their ages are as follows:
+# Anton is 21 years old.
+# Beth is 6 years older than Anton.
+# Chen is 20 years older than Beth.
+# Drew is as old as Chen's age plus Anton's age.
+# Ethan is the same age as Chen.
+# Your code should store each person's age to a variable and print their names and ages at the end.
+# Anton is 3
+# Beth is 4
+# Chen is 5
+# Drew is 6
+# Ethan is 7
 
-# Prompt the user to enter the second number
-second_number = int(input("Enter the second number: "))
 
-# Calculate the sum of the two numbers
-total_sum = first_number + second_number
+# Assigning ages based on the riddle
 
-# Print the total sum with an appropriate message
-print(f"The total sum is: {total_sum}")
+Anton_age:int = 21
+Beth_age:int = 27
+Chen_age:int = 47
+Drew_age:int = 68
+Ethan_age:int = Chen_age
+
+print(f"Anton is {Anton_age}")
+print(f"Beth is {Beth_age}")
+print(f"Chen is {Chen_age}")
+print(f"Drew is {Drew_age}")
+print(f"Ethan is {Ethan_age}")
